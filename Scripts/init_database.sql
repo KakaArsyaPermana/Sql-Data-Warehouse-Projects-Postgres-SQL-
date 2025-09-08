a7999a817530132ee3e5eedbaa10dae20ef0a622
KakaArsyaPermana <kakaarsyapermana12@gmail.com>
2025-09-08 21:08:39 +0700

    Update init_database.sql

--- Scripts/init_database.sql
+++ Scripts/init_database.sql
@@ -3,23 +3,41 @@
 
 */
 
+-- noted dilakukan pada query pada database bawaan postgressql yaitu postgres  (jika belum ada database sama sekali) --
+-- Memeriksa apakah nama database yang ingin dibuat sudah ada atau belum --
+DO $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT FROM pg_database WHERE datname = 'datawarehouse'
+  ) THEN
+    RAISE NOTICE 'Database belum ada';
+  ELSE 
+    RAISE NOTICE 'Database sudah ada';
+  END IF;
+END$$ 
+LANGUAGE plpgsql;
 
-Mengecek apakah nama database yang ingin dibuat sudah ada atau belum
-SELECT datname
-FROM pg_database
-WHERE datname = 'datawarehouse';
+-- Membuat role owner untuk database --
+create role de_owner with 
+	login 
+	password  'de_owner'
 
--- membuat Database 'DataWarehouse'
-create database DataWarehouse; 
+-- Membuat database dengan owner de_owner -- 
+create database datawarehouse with
+owner ='de_owner'
 
--- membuat schema dari semua layer -- 
--- extract layer -- 
-create schema extract;
--- extract transform -- 
-create schema transform;
+-- Noted dilakukan query pada database datawarehouse dengan login menggunakan role de_owner --
 
--- extract Modeling -- 
-create schema Modeling;
+-- Membuat shcema untuk proses etl di stagging area dengan format (stg_namalayer)  dan warehouse dengan format (wh_namalayer)--
+-- schema extract -- 
+create schema stg_extract 
 
--- extract security -- 
-create schema security;
+-- schema transfrom -- 
+create schema stg_transfrom  
+
+-- schema modeling -- 
+create schema wh_modeling 
+
+-- schema security -- 
+create schema wh_security 
+
